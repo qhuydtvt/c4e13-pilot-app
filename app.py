ce61721775081551623e968e01fade2d004d6c24
huynq <qhuydtvt@gmail.com>
2017-12-11 19:42:11 +0700

    session 11

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mlab
 from mongoengine import *
 
@@ -19,6 +19,25 @@ class Item(Document):
 def index():
     items = Item.objects() # Get ALL items
     return render_template('index.html', items=items)
+
+
+@app.route('/add_item', methods=['GET', 'POST'])
+def add_item():
+    if request.method == "GET": # Get form
+        return render_template('add_item.html')
+    elif request.method == "POST": # Receive form
+        # 1 Extract data in form
+        form = request.form
+        title = form['title']
+        image = form['image']
+        description = form['description']
+        price = form['price']
+
+        # 2 Add into database
+        new_item = Item(title=title, image=image, description=description, price=price)
+        new_item.save() # Save into database
+
+        return "OKe anh"
 
 
 if __name__ == '__main__':
